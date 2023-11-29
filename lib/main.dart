d37c1e6addf4415c547b1eed522b1a692fbcba0d
Logards <bastien.ricci@ynov.com>
2023-11-29 10:23:22 +0100

    Change button emplaceemnt

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
     return ChangeNotifierProvider(
       create: (context) => MyAppState(),
       child: MaterialApp(
+        debugShowCheckedModeBanner: false,
         title: 'Share Everything',
         theme: ThemeData(
           useMaterial3: true,
@@ -33,7 +34,6 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appState = context.watch<MyAppState>();
-
     return Scaffold(
       backgroundColor: Color.fromRGBO(28, 28, 28, 1),
       body: Column(
@@ -47,6 +47,7 @@ class MyHomePage extends StatelessWidget {
                     MaterialStateProperty.all(Color.fromRGBO(56, 182, 255, 1))),
             onPressed: () {
               print("Se connecte");
+              Text("Salut");
             },
             child: Text(
               "Se connecter",
@@ -54,17 +55,34 @@ class MyHomePage extends StatelessWidget {
             ),
           )),
           Spacer(),
-          Image(
-            image: AssetImage('assets/logo.ico'),
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            child: Image(
+              image: ResizeImage(AssetImage('assets/logo.ico'),
+                  width: 70, height: 65),
+            ),
           ),
-          Text(
-            "You are known as Logards",
-            style: TextStyle(color: Colors.grey),
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            child: RichText(
+              text: TextSpan(
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  children: <TextSpan>[
+                    TextSpan(text: "You are known as "),
+                    TextSpan(
+                        text: "Logards", style: TextStyle(color: Colors.orange))
+                  ]),
+            ),
           ),
-          Text(
-            "You can be discovered by everyone on this network",
-            style: TextStyle(color: Colors.blue),
-          ),
+          Container(
+            margin: EdgeInsets.only(bottom: 10),
+            child: Text(
+              "You can be discovered by everyone on this network",
+              style: TextStyle(color: Colors.blue),
+            ),
+          )
         ],
       ),
     );
