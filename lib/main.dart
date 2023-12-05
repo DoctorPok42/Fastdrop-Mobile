3728e7d9f34c268b77aaa75e6d5946acc6febb47
Logards <bastien.ricci@ynov.com>
2023-12-05 09:01:33 +0100

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:provider/provider.dart';
 
 void main() {
